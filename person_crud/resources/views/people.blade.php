77bc83aad3430958534b18f1b905ca0ce2b7e3a7
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-12-01 20:21:24 +0000

    chore: opcache ini

--- person_crud/resources/views/people.blade.php
+++ person_crud/resources/views/people.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Pessoas</title>
-    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
